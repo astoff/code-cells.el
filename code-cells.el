f93db2d65cab6f07a03bb85660f6500cdf44fb86
Augusto Stoffel <arstoffel@gmail.com>
2021-09-25 17:31:16 +0200

    Refinement in Commentary section

--- code-cells.el
+++ code-cells.el
@@ -30,13 +30,24 @@
 ;; back to notebook format when saving.  An external tool, Jupytext by
 ;; default, is required for this.
 ;;
-;; Out of the box, there are no keybindings and, in fact, only a small
-;; number of editing commands is provided.  Rather, the idea is that
-;; you can create your own cell-aware commands from regular ones
-;; through the `code-cells-command' function and the `code-cells-do'
-;; macro.  See the README for configuration examples.  There is also a
-;; `code-cells-mode' minor mode, which, among other things, provides
-;; outline support.
+;; A minor mode, `code-cells-mode`, provides the following features:
+;;
+;; - Fontification of cell boundaries.
+;;
+;; - Keybindings for the cell navigation and evaluation commands,
+;;   under the `C-c %' prefix.
+;;
+;; - Outline mode integration: cell headers have outline level
+;;   determined by the number of percent signs or asterisks; within a
+;;   cell, outline headings are as determined by the major mode, but
+;;   they are demoted by an amount corresponding to the level of the
+;;   containing cell.  This provides code folding and hierarchical
+;;   navigation, among other things, when `outline-minor-mode` is
+;;   active.
+;;
+;; This minor mode is automatically activated when opening an ipynb
+;; file, but you can also activate it in any other buffer, either
+;; manually or through some hook.
 
 ;;; Code:
 
