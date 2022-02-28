ea7799c447066fee78c4efbafbdaf09520c7109d
Augusto Stoffel <arstoffel@gmail.com>
2022-02-28 21:39:17 +0100

    Release on ELPA

--- code-cells.el
+++ code-cells.el
@@ -1,12 +1,12 @@
-;;; code-cells.el --- Work with code split into cells, including Jupyter notebooks -*- lexical-binding: t; -*-
+;;; code-cells.el --- Lightweight notebooks with support for ipynb files -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021 Augusto Stoffel
+;; Copyright (C) 2022  Free Software Foundation, Inc.
 
 ;; Author: Augusto Stoffel <arstoffel@gmail.com>
 ;; Keywords: convenience, outlines
 ;; URL: https://github.com/astoff/code-cells.el
 ;; Package-Requires: ((emacs "27.1"))
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -25,12 +25,12 @@
 
 ;; With this package, you can efficiently navigate, edit and execute
 ;; code split into cells according to certain magic comments.  It also
-;; allows to open ipynb notebook files directly in Emacs.  They will
-;; be automatically converted to a script for editing, and converted
-;; back to notebook format when saving.  An external tool, Jupytext by
-;; default, is required for this.
+;; allows you to open ipynb notebook files directly in Emacs.  They
+;; will be automatically converted to a script for editing, and
+;; converted back to notebook format when saving.  An external tool,
+;; Jupytext by default, is required for this.
 ;;
-;; A minor mode, `code-cells-mode`, provides the following features:
+;; A minor mode, `code-cells-mode', provides the following features:
 ;;
 ;; - Fontification of cell boundaries.
 ;;
@@ -42,12 +42,12 @@
 ;;   cell, outline headings are as determined by the major mode, but
 ;;   they are demoted by an amount corresponding to the level of the
 ;;   containing cell.  This provides code folding and hierarchical
-;;   navigation, among other things, when `outline-minor-mode` is
+;;   navigation, among other things, when `outline-minor-mode' is
 ;;   active.
 ;;
 ;; This minor mode is automatically activated when opening an ipynb
 ;; file, but you can also activate it in any other buffer, either
-;; manually or through some hook.
+;; manually or through a hook.
 
 ;;; Code:
 
